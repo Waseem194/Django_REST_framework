c3611a097f6dd191722bd816bb08c67cc6415652
Waseem194 <bsf1801678@ue.edu.pk>
2024-06-13 16:23:41 +0500

    classBase

--- imdb/imdb_app/api/urls.py
+++ imdb/imdb_app/api/urls.py
@@ -1,9 +1,15 @@
 # from django.contrib import admin
 from django.urls import path
-from imdb_app.api.views import movie_list, movie_detail
+
+
+# from imdb_app.api.views import movie_list, movie_detail
+from imdb_app.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('list/', movie_list, name="movie_list"),
-    path('<int:pk>/', movie_detail, name="movie_detail"),
+    # path('list/', movie_list, name="movie_list"),
+    # path('<int:pk>/', movie_detail, name="movie_detail"),
+    
+    path('list/', MovieListAV.as_view(), name="movie_list"),
+    path('<int:pk>/',  MovieDetailAV.as_view(), name="movie_detail"),
 ]
