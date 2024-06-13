bbd0202f06e31508daa66da967d2275b81939b0f
Waseem194 <bsf1801678@ue.edu.pk>
2024-06-13 13:58:30 +0500

    drf

--- imdb/imdb_app/api/urls.py
+++ imdb/imdb_app/api/urls.py
@@ -4,6 +4,6 @@ from imdb_app.api.views import movie_list, movie_detail
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('list', movie_list, name="movie_list"),
-    path('<int:pk>', movie_detail, name="movie_detail"),
+    path('list/', movie_list, name="movie_list"),
+    path('<int:pk>/', movie_detail, name="movie_detail"),
 ]
--- imdb/imdb_app/api/views.py
+++ imdb/imdb_app/api/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -25,22 +25,26 @@ def movie_list(request):
 
 def movie_detail(request,pk):
     if request.method == "GET":
-        movie = Movie.objects.get(pk= pk)
+        try:
+            movie = Movie.objects.get(pk= pk)
+        except Movie.DoesNotExist:
+            return Response({'Error': 'Movie not found'},status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     
     if request.method == "PUT":
         movie = Movie.objects.get(pk = pk)
-        
-        serializer = MovieSerializer(data = request.data)
+        serializer = MovieSerializer(movie, data = request.data)
         if serializer.is_valid():
-            serializer.update()
+            serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors,status=400)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
         
-    # if request.method == "DELETE":
-    #     serializer.delete()
-    #     return Response()
+        
+    if request.method == "DELETE":
+        movie = Movie.objects.get(pk = pk)
+        movie.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
         
         
